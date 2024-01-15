e443a45b349e5a20804b09b156b3711ef098eaeb
LeSaR <ilkov2006@gmail.com>
2024-01-15 14:48:41 +0300

    added buying items from the shop and inventory

--- header.h
+++ header.h
@@ -14,4 +14,9 @@ bool load_shop(std::string const& dir);
 
 bool user_input();
 
+int accessible_shop_level();
 void show_shop();
+bool buy_item(int shop_level, unsigned int index);
+void clear_shop_level(int shop_level);
+
+void show_inventory();
